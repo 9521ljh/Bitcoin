5eacaa08336368f7992bb976e07924639357fa0b
9521ljh <9521ljh@gmail.com>
2022-08-04 03:38:42 +0000

    datetime

--- parser.py
+++ parser.py
@@ -2,21 +2,21 @@ import pandas as pd
 
 df = pd.read_csv('1d.csv')
 
-dict_ = {}
-subdict = {}
+dict_ = {'symbol': 'AAPL'}
+# subdict = {'symbol': 'AAPL'}
+klines = []
 for idx in range(len(df)):
-    subdict = {
-        f"{df.iloc[idx]['datetime']}" : {
+    klines.append({
+        'datetime': str(df.iloc[idx]['datetime']),
         'open' : float(df.iloc[idx]['open']),
         'high' : float(df.iloc[idx]['high']),
         'low' : float(df.iloc[idx]['low']),
         'close' : float(df.iloc[idx]['close']),
-        'volume' : float(df.iloc[idx]['volume']),
-        }
-    }
-    dict_.update(subdict)
+        'volume' : float(df.iloc[idx]['volume']),        
+    })
+    dict_.update({'klines': klines})
 
 import json
 
-with open('AAPL.json', 'w') as fp:
+with open('AAPL_datetime.json', 'w') as fp:
     json.dump(dict_, fp)
